94600b4f77b29851cb2e21655d9df0875751b2df
Sophia Iroegbu <66218403+Sophyia7@users.noreply.github.com>
2022-07-29 16:48:56 +0100

    Update views.py
    Changed message to detail on the response objects

--- accounts/views.py
+++ accounts/views.py
@@ -28,7 +28,8 @@ class Register(GenericAPIView):
         if num_results > 0:
             return Response(
                 {
-                    "message": "User already exists",
+                    "success" False,
+                    "detail": "User already exists",
                 },
                 status=status.HTTP_400_BAD_REQUEST,
             )
@@ -57,13 +58,15 @@ class Register(GenericAPIView):
 
             return Response(
                 {
-                    "message": "User created successfully.",
+                    "success": True,
+                    "detail": "User created successfully.",
                 },
                 status=status.HTTP_201_CREATED,
             )
         return Response(
             {
-                "message": "User already exists",
+                "success" False,
+                "detail": "User already exists",
             },
             status=status.HTTP_400_BAD_REQUEST,
         )
@@ -81,7 +84,8 @@ class Login(GenericAPIView):
         except User.DoesNotExist:
             return Response(
                 {
-                    "message": "Email or password is incorrect",
+                    "success": False,
+                    "detail": "Email or password is incorrect",
                 },
                 status=status.HTTP_401_UNAUTHORIZED,
             )
@@ -94,7 +98,8 @@ class Login(GenericAPIView):
                 refresh = RefreshToken.for_user(user)
                 return Response(
                     {
-                        "message": "Login Successful",
+                        "success": True,
+                        "detail": "Login Successful",
                         "data": {
                             "user": {
                                 "user_id": user.pk,
@@ -108,14 +113,16 @@ class Login(GenericAPIView):
             else:
                 return Response(
                     {
-                        "message": "User is not active",
+                        "success": False,
+                        "detail": "User is not active",
                     },
                     status=status.HTTP_401_UNAUTHORIZED,
                 )
         else:
             return Response(
                 {
-                    "message": "Email or password is not correct.",
+                    "success": False,
+                    "detail": "Email or password is not correct.",
                 },
                 status=status.HTTP_401_UNAUTHORIZED,
             )
@@ -129,12 +136,18 @@ class ResendActivationLink(GenericAPIView):
             user = User.objects.get(email=email)
         except User.DoesNotExist:
             return Response(
-                {"message": "User does not exist"},
+                {
+                    "success": False,
+                    "detail": "User does not exist"
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
         if user.is_active:
             return Response(
-                {"message": "User is already active"},
+                {
+                    "success": False,
+                    "detail": "User is already active"
+               },
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -154,7 +167,10 @@ class ResendActivationLink(GenericAPIView):
         send_mail(subject, message, from_email, to_email)
 
         return Response(
-            {"message": "Email sent", "data": message},
+            {
+                "success" True,
+                "detail": "Email sent", "data": message
+            },
             status=status.HTTP_200_OK,
         )
 
@@ -166,19 +182,28 @@ class ActivateEmail(GenericAPIView):
             user = User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return Response(
-                {"message": "User does not exist"},
+                {
+                    "success": False,
+                    "detail": "User does not exist"
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
         if user and account_activation_token.check_token(user, token):
             user.is_active = True
             user.save()
             return Response(
-                {"message": "Email activated successfully"},
+                {
+                    "success": True,
+                    "detail": "Email activated successfully"
+               },
                 status=status.HTTP_200_OK,
             )
         else:
             return Response(
-                {"message": "Invalid token"},
+                {
+                    "success": False,
+                    "detail": "Invalid token"
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -191,7 +216,10 @@ class ResetPassword(GenericAPIView):
             user = User.objects.get(email=email)
         except User.DoesNotExist:
             return Response(
-                {"message": "User does not exist."}, status=status.HTTP_401_UNAUTHORIZED
+                {
+                    "success": False,
+                    "detail": "User does not exist."
+                }, status=status.HTTP_401_UNAUTHORIZED
             )
 
         if user.is_active:
@@ -213,14 +241,18 @@ class ResetPassword(GenericAPIView):
 
             return Response(
                 {
-                    "message": "Check your mail to reset your password",
+                    "success": True,
+                    "detail": "Check your mail to reset your password",
                     "data": message
                 },
                 status=status.HTTP_200_OK,
             )
         else:
             return Response(
-                {"message": "User is not active. Request for an activation link."},
+                {
+                    "success": False,
+                    "detail": "User is not active. Request for an activation link."
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -234,7 +266,10 @@ class ConfirmPassword(GenericAPIView):
             user = User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return Response(
-                {"message": "User does not exist"},
+                {
+                    "success": False,
+                    "detail": "User does not exist"
+                },
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -245,15 +280,22 @@ class ConfirmPassword(GenericAPIView):
                 user.save()
 
                 return Response(
-                    {"message": "New password set."}, status=status.HTTP_200_OK
+                    {
+                        "success": True,
+                        "detail": "New password set."
+                    }, status=status.HTTP_200_OK
                 )
             else:
                 return Response(
-                    {"data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+                    {
+                        "success": False,
+                        "detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
                 )
         else:
             return Response(
-                {"message": "Password Reset Failed!"},
+                {
+                    "success": False,
+                    "detail": "Password Reset Failed!"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
@@ -263,6 +305,8 @@ class Logout(GenericAPIView):
         logout(request)
 
         return Response(
-            {"message": "Logout Successful"},
+            {
+                "success": True,
+                "detail": "Logout Successful"},
             status=status.HTTP_200_OK,
         )

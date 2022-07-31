97245a1a9e9b710dd8cad55a6f6e5baf252e4ba4
Sophyia7 <iroegbusophia3@gmail.com>
2022-07-31 21:20:36 +0100

    Adjusted jwt acces tken timer

--- mysite/settings.py
+++ mysite/settings.py
@@ -220,7 +220,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False, 
     'BLACKLIST_AFTER_ROTATION': True, 
